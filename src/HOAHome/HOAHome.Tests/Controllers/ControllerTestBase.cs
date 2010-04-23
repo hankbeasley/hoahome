77e22970ccb42c73cefaa2f8e156ed68ede5cda3
hank beasley <hankbeasley@yahoo.com>
2010-04-23 16:53:09 -0500

    stuff

---
+++ src/HOAHome/HOAHome.Tests/Controllers/ControllerTestBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HOAHome.Code.EntityFramework;
+using HOAHome.Controllers;
+using HOAHome.Code.Mvc;
+
+namespace HOAHome.Tests.Controllers
+{
+    public class ControllerTestBase<T,Q> where T: CustomController<Q>, new() where Q: IEntity, new()
+    {
+        private T controller = new T();
+
+        protected Moq.Mock<IPersistanceFramework> GetPersistanceMock()
+        {
+            var mock = new Moq.Mock<IPersistanceFramework>();
+            CustomController_Accessor<Q>.AttachShadow(controller).Persistance = mock.Object;
+            return mock;
+        }
+
+        protected T Controller
+        {
+            get
+            {
+                return this.controller;
+            }
+        }
+    }
+}
